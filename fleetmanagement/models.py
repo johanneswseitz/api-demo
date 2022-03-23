b70239f4f7dfba4340ae4426d24ea94c42a56202
Johannes Seitz <859551+johanneswseitz@users.noreply.github.com>
2022-03-23 16:10:44 +0100

    Fake generation

--- fleetmanagement/models.py
+++ fleetmanagement/models.py
@@ -9,6 +9,7 @@ STATUSES = [
     ('FAHRBEREIT', 'Fahrbereit'),
 ]
 
+
 class Auto(models.Model):
     hersteller = models.CharField(max_length=40)
     modell = models.CharField(max_length=40)
@@ -18,3 +19,7 @@ class Auto(models.Model):
     def __str__(self):
         return f"{self.hersteller}  {self.modell}"
 
+
+if len(Auto.objects.all()) == 0:
+    print("DATABASE IS EMPTY; GENERATING FAKE ENTRIES")
+    Auto(hersteller="Tesla", modell="Model 3", nummernschild="", status=STATUSES[0][0]).save()
