ee0de6b2cf7c6db929df459d9d41bf51ab09aae4
Johannes Seitz <859551+johanneswseitz@users.noreply.github.com>
2022-03-23 15:44:44 +0100

    Initial rest trials

--- apidemo/urls.py
+++ apidemo/urls.py
@@ -14,8 +14,46 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import routers, serializers, viewsets
+from fleetmanagement.models import Auto
+
+
+class AutoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Auto
+        fields = ["hersteller", "modell", "nummernschild", "status"]
+
+# ViewSets define the view behavior.
+class AutoViewSet(viewsets.ModelViewSet):
+    queryset = Auto.objects.all()
+    serializer_class = AutoSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'autos', AutoViewSet)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Fleet Management API",
+        default_version='v1',
+        description="API um das Flottenmanagement zu automatisieren",
+        terms_of_service="Bitte nix kaputt machen :)",
+        contact=openapi.Contact(email="contact@fleetmanagement.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
